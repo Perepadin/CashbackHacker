5ea9fb5399c84e115003adaa290f264dd6816ce2
Perepadin <82140378+Perepadin@users.noreply.github.com>
2021-07-17 23:48:17 +0300

    Initial Commit 3 junit4

---
+++ src/test/java/ru/netology/CashbackServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackServiceTest {
+
+    private CashbackService cashbackService = new CashbackService();
+
+    @Test
+    public void more1000() {
+        assertEquals(400, cashbackService.remain(5600));
+    }
+
+    @Test
+    public void low1000() {
+        assertEquals(850, cashbackService.remain(150));
+    }
+
+    @Test
+    public void underZero() {
+        // негативный падающий тест
+        assertEquals(1150, cashbackService.remain(-150));
+    }
+
+    @Test
+    public void exactly1000() {
+        //  падающий тест
+        assertEquals(1000, cashbackService.remain(1000));
+    }
+}
